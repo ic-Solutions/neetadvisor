7873db150feb19488c41446ea9b1bd4cb134e237
sharathdt <sharath.dt@gmail.com>
2021-07-30 23:09:44 +0530

    change contact form mail sender

--- contact-form-mail-sender.php
+++ contact-form-mail-sender.php
@@ -28,10 +28,10 @@ if(isset($_POST['submit'])){
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'sg2plcpnl0113.prod.sin2.secureserver.net';        // Set the SMTP server to send through
+    $mail->Host       = 'cs3002.hostgator.in';        // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'submissions@leopride.in';                     // SMTP username
-    $mail->Password   = ';a~VtLxho.5!';                               // SMTP password
+    $mail->Username   = 'neetamls';                     // SMTP username
+    $mail->Password   = 'xU7CLq4Fu25Pec';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
