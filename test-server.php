e77459430ecdb13483c6dd6e7c4228a82f587c9d
akash <dakash682@gmail.com>
2022-07-19 15:38:35 +0530

    stress management blog added

--- test-server.php
+++ test-server.php
@@ -1,13 +1,5 @@
----
-title: test-s
-description: NEET Advisor provides a total career counselling where we have come together to help, guide and counsel students in every aspect from making the right career choice, choosing the right course/institution and even help you settle down in new environs.
-image: /assets/images/4-1.jpg
-layout: default
-permalink: /test-s/
----
-
 <?php
 $name = $_POST["marks"];
 
-echo $sum
+echo $name
 ?>
